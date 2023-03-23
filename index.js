15004628abaad50703b861816261d8d8550187b1
Matthew Auer <matthewauer72@gmail.com>
2023-03-23 12:05:30 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun (activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun("bathe my dog")
+
+mondayWork = function (activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+mondayWork("work from home")
+
+function wrapAdjective (string = "*" ){ 
+    return function (value = "special"){
+        return `You are ${string}${value}${string}!`
+}
+}
